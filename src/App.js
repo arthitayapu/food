3528a685f797e2e469672ea834be3900aa46cfcb
arthitayapu <atyhitaya.pu.65@ubu.ac.th>
2023-07-25 11:08:22 +0700

    jft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, {useRef} from "componennt Menu";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { Container, Button, Table } from "react-bootstrap";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Contact from "./Contact";
+import 
 
-function App() {
-  return (
+return (
+  <userContext.Provider value={"สมชาย"}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <Header />
+      <Content />
+      <div>
+        <h3>จำนวนสินค้าในตะกร้า {totalItems}</h3>
+        <h3>ราคาสินค้าในตะกร้าทั้งหมด {totalPrice} บาท</h3>
+      </div>
+      <div>
+        <img src="case.jpg" alt="case" width="15%" />
+        <br />
+        เคสโทรศัพท์ ราคา {product1} บาท
+      </div>
+      <button
+        onClick={() => {
+          setTotalItems(totalItems + 1);
+          setTotalPrice(totalPrice + product1);
+        }}
+      >
 
-export default App;
+export function Layout() {
+  return (
+  <nav className="bg-dark p-2 mb-3 text-center">
+    <NavLink to="/" className="link px-2" style={({isActive}) => {
+      return {
+        textDecoration : isActive ? "none" : "underline",
+      };
+    }}>Main</NavLink>
+    <NavLink to="/product" className="link px-2" style={({isActive}) => {
+      return {
+        textDecoration : isActive ? "none" : "underline",
+      };
+    }}>Product</NavLink>
+    <NavLink to="/contact" className="link px-2" style={({isActive}) => {
+      return {
+        textDecoration : isActive ? "none" : "underline",
+      };
+    }}>Contact Us</NavLink>
+  </nav>
+  );
+} 
+
